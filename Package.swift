64a706bf724b1a0da2ce630d4234875e57641a90
Tim Vermeulen <tvermeulen@me.com>
2020-03-19 18:42:53 +0100

    Add library product

--- Package.swift
+++ Package.swift
@@ -2,4 +2,22 @@
 
 import PackageDescription
 
-let package = Package(name: "Parser")
+let package = Package(
+    name: "Parser",
+    products: [
+        .library(
+            name: "Parser",
+            targets: ["Parser"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "Parser",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "ParserTests",
+            dependencies: ["Parser"]
+        ),
+    ]
+)
